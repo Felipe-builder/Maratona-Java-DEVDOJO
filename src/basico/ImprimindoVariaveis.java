ef0f56ff7ff1733d2686c4d245c412ea3efbc5ec
Felipe-builder <felipesvascon@gmail.com>
2021-12-06 13:00:31 -0300

    Primeira Aula, Tipos Primitivos

---
+++ src/basico/ImprimindoVariaveis.java
@@ -0,0 +1,22 @@
+package basico;
+
+public class ImprimindoVariaveis {
+    // comentário de uma única linha
+
+    /*
+    Comentário de mais de uma linha
+     */
+
+    /**
+     * Compentário oficial
+     * @param args
+     */
+    public static void main(String[] args) {
+        int idade = 12;
+        double peso = 46.7;
+        float tamanho = 1.40F;
+        boolean maiorDeIdade = false;
+        System.out.println("Dados Impressos");
+        System.out.println("Idade: " + idade);
+    }
+}
---
+++ src/basico/OlaMundo.java
@@ -0,0 +1,7 @@
+package basico;
+
+public class OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("Olá diretamente do IntelliJ");
+    }
+}
