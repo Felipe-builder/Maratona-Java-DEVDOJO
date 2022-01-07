7ca47d2e40c0cfb9869e96abe69d6381a0e492c1
Felipe-builder <felipesvascon@gmail.com>
2022-01-07 17:07:20 -0300

    Arrays

--- src/basico/Arrays3.java
+++ src/basico/Arrays3.java
@@ -1,2 +1,21 @@
-package basico;public class Arrays3 {
+package basico;
+
+public class Arrays3 {
+    public static void main(String[] args) {
+        int[] numeros = new int[5];
+        int[] numeros2 = {
+            1, 2, 3, 4, 5
+        };
+
+        int[] numeros3 = new int[] {1,2,3,4,5};
+
+
+        for(int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+
+        for(int valor : numeros2) {
+            System.out.println(valor);
+        }
+    }
 }
