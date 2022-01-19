38ed4f39aca9fd2a6f6442a1cdd43e090cca6978
Felipe-builder <felipesvascon@gmail.com>
2022-01-18 22:02:58 -0300

    foreach

---
+++ src/basico/Arrays4.java
@@ -0,0 +1,10 @@
+package basico;
+
+public class Arrays4 {
+    public static void main(String[] args) {
+        String[] nomes = {"Juju", "João João", "Xu Xu"};
+        for (String nome: nomes) {
+            System.out.println(nome);
+        }
+    }
+}
